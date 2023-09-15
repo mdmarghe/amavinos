c8c9531ee1eb18a535ed45412f3e01f1eb8a8150
mdmarghe <margheritanuccio98@gmail.com>
2023-09-15 17:53:18 +0200

    ok

--- store/admin.py
+++ store/admin.py
@@ -1,22 +1,9 @@
 from django.contrib import admin
-from .models.product import Products
-from .models.category import Category
-from .models.customer import Customer
-from .models.orders import Order
 
+from .models import *
 
-class AdminProduct(admin.ModelAdmin):
-    list_display = ['name', 'price', 'category']
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
-
-# Register your models here.
-admin.site.register(Products,AdminProduct)
-admin.site.register(Category)
 admin.site.register(Customer)
+admin.site.register(Product)
 admin.site.register(Order)
-
-
-# username = Tanushree, email = tanushree7252@gmail.com, password = 1234
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
--- store/urls.py
+++ store/urls.py
@@ -1,23 +1,14 @@
-from django.contrib import admin
 from django.urls import path
-from .views.home import Index , store
-from .views.signup import Signup
-from .views.login import Login , logout
-from .views.cart import Cart
-from .views.checkout import CheckOut
-from .views.orders import OrderView
-from .middlewares.auth import  auth_middleware
 
+from . import views
 
 urlpatterns = [
-    path('', Index.as_view(), name='homepage'),
-    path('store', store , name='store'),
+	#Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
 
-    path('signup', Signup.as_view(), name='signup'),
-    path('login', Login.as_view(), name='login'),
-    path('logout', logout , name='logout'),
-    path('cart', auth_middleware(Cart.as_view()) , name='cart'),
-    path('check-out', CheckOut.as_view() , name='checkout'),
-    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
 
 ]
