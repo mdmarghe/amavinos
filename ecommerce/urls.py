e9aec2e65730be997f2a1c56ba6ceeb9cf587f94
mdmarghe <margheritanuccio98@gmail.com>
2023-09-20 01:05:59 +0200

    provando a configurare stripe

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path("stripe/", include("djstripe.urls", namespace="djstripe")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
